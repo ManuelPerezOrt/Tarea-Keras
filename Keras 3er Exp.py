1ead6153e6b8547da90ec3ecf3076ba4f990c5b9
ManuelPerezOrt <88348058+ManuelPerezOrt@users.noreply.github.com>
2024-02-12 01:27:59 -0600

    3er Experimento parte b)
    En este 3er experimento coloque cinco capas intermedias, comenzando con 500 neuronas reduciendo a la siguiente a la mitad hasta llegar a 30, use un tamaño de mini banch de 120 y solo use la función de activación sigmoide, utilice el categorical cross entropy con el optimizador Adam. Como resultado obtuve un accuracy casi del 100% por lo que fue mejor. Vi que si reducía las capas a las originales llegaba al 100% de accuracy

---
+++ Keras 3er Exp.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+learning_rate = 0.001
+epochs = 30
+batch_size = 120
+
+import mlflow
+mlflow.tensorflow.autolog()
+dataset=mnist.load_data()
+
+(x_train, y_train), (x_test, y_test) = dataset
+
+x_trainv = x_train.reshape(60000, 784)
+x_testv = x_test.reshape(10000, 784)
+
+x_trainv = x_trainv.astype('float32')
+x_testv = x_testv.astype('float32')
+x_trainv /= 255 
+x_testv /= 255
+
+num_classes=10
+y_trainc = keras.utils.to_categorical(y_train, num_classes)
+y_testc = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(500, activation='sigmoid', input_shape=(784,)))
+model.add(Dense(250, activation='sigmoid'))
+model.add(Dense(125, activation='sigmoid'))
+model.add(Dense(60, activation='sigmoid'))
+model.add(Dense(30, activation='sigmoid'))
+model.add(Dense(num_classes, activation='sigmoid'))
+model.summary()
+model.compile(loss='categorical_crossentropy',optimizer=Adam(learning_rate=learning_rate),metrics=['accuracy'])
+history = model.fit(x_trainv, y_trainc,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_testv, y_testc)
+                    )
+
+score = model.evaluate(x_testv, y_testc, verbose=1)
+print(score)
+a=model.predict(x_testv)
+#b=model.predict_proba(x_testv)
+print(a.shape)
+print(a[1])
+print("resultado correcto:")
+print(y_testc[1])
+
+model.save("red.h5")
+exit()
+modelo_cargado = tf.keras.models.load_model('red.h5')
