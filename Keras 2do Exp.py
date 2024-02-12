cf27cf8dbfa1a92d4f2e1b7a38c417784f13de64
ManuelPerezOrt <88348058+ManuelPerezOrt@users.noreply.github.com>
2024-02-12 01:10:29 -0600

    2do Exp parte b)
    En este 2do experimento coloque siete capas intermedias, llendo de 700 neuronas hasta 100 en la penúltima capa bajando de 100 en 100, use un tamaño de mini banch de 400 sin embargo también lo probé con 100 y solo use la función de activación softmax, tambien cambie la binary cross entropy por la categorical  y use el optimizador SGD. Como resultado obtuve un accuracy de alrededor del 1% esto ya que se quedó trabado el código debido a un error de sobreajuste no avanzaba mas el accuracy.

---
+++ Keras 2do Exp.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import RMSprop, SGD
+
+learning_rate = 0.001
+epochs = 30
+batch_size = 100
+
+import mlflow
+mlflow.tensorflow.autolog()
+dataset=mnist.load_data()
+
+(x_train, y_train), (x_test, y_test) = dataset
+
+x_trainv = x_train.reshape(60000, 784)
+x_testv = x_test.reshape(10000, 784)
+
+x_trainv = x_trainv.astype('float32')
+x_testv = x_testv.astype('float32')
+x_trainv /= 255 
+x_testv /= 255
+
+num_classes=10
+y_trainc = keras.utils.to_categorical(y_train, num_classes)
+y_testc = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(700, activation='softmax', input_shape=(784,)))
+model.add(Dense(600, activation='softmax'))
+model.add(Dense(500, activation='softmax'))
+model.add(Dense(400, activation='softmax'))
+model.add(Dense(300, activation='softmax'))
+model.add(Dense(200, activation='softmax'))
+model.add(Dense(100, activation='softmax'))
+model.add(Dense(num_classes, activation='softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy',optimizer=SGD(learning_rate=learning_rate),metrics=['accuracy'])
+history = model.fit(x_trainv, y_trainc,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_testv, y_testc)
+                    )
+
+score = model.evaluate(x_testv, y_testc, verbose=1)
+print(score)
+a=model.predict(x_testv)
+#b=model.predict_proba(x_testv)
+print(a.shape)
+print(a[1])
+print("resultado correcto:")
+print(y_testc[1])
+
+model.save("red.h5")
+exit()
+modelo_cargado = tf.keras.models.load_model('red.h5')
